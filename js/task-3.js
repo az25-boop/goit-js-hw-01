a11691aa9096821527caedd140c66c8ea9f8a62b
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-05-09 18:15:33 +0300

    task123

--- js/task-3.js
+++ js/task-3.js
@@ -6,6 +6,6 @@ function getElementWidth(content, padding, border) {
   return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
