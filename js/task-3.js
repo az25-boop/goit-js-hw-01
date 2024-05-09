8e1ddaaab93258f59e6abe7ab63ee6a6c678ed2a
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-05-09 13:43:35 +0300

    task1-2-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,3 @@
-```
 function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
@@ -10,4 +9,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-```;
